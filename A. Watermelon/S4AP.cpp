edacbf799ef65640a14eb9632afc542cc25a4d5e
Kyrillos Walid Radi <30301905+KOKADev@users.noreply.github.com>
2018-05-07 03:43:26 +0200

    Add files via upload

---
+++ A. Watermelon/S4AP.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+	/*
+        **********************************************************
+        * THIS CODE BY KOKADEV...KYRILLOSWALIDRADI               *
+        * ALL COPYRIGHYS ARE SAVED TO KYRILLOSWALIDRADI          *
+        * + 2 0 1 0 6 2 7 0 4 4 1 3 - + 2 0 1 2 1 1 2 8 0 3 5 8  *
+        * KYRILLOSWALIDRADI@HOTMAIL.COM                          *
+        * WWW.FACEBOOK.COM/KOKADEV                               *
+        * WWW.GITHUB.COM/KOKADev                                 *
+        **********************************************************
+        */
+	int x;
+	cin>>x;
+	if((x%2==0)&&(x>2)){
+		cout<<"YES";
+	}
+	else{
+		cout<<"NO";
+	}
+	return 0;
+}
