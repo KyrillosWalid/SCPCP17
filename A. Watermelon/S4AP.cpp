04ad4d7a60965a2df490c6f51446e2a05a0b44c5
Kyrillos Walid Radi <30301905+KyrillosWalid@users.noreply.github.com>
2018-11-04 18:44:56 +0200

    Update S4AP.cpp

--- A. Watermelon/S4AP.cpp
+++ A. Watermelon/S4AP.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 int main() {
-	/*
-        **********************************************************
-        * THIS CODE BY KOKADEV...KYRILLOSWALIDRADI               *
-        * ALL COPYRIGHYS ARE SAVED TO KYRILLOSWALIDRADI          *
-        * + 2 0 1 0 6 2 7 0 4 4 1 3 - + 2 0 1 2 1 1 2 8 0 3 5 8  *
-        * KYRILLOSWALIDRADI@HOTMAIL.COM                          *
-        * WWW.FACEBOOK.COM/KOKADEV                               *
-        * WWW.GITHUB.COM/KOKADev                                 *
-        **********************************************************
-        */
+   /*
+   **********************************************************
+   * THIS CODE BY KYRILLOSWALIDRADI                         *
+   * ALL COPYRIGHYS ARE SAVED TO KYRILLOSWALIDRADI          *
+   * + 2 0 1 0 6 2 7 0 4 4 1 3 - + 2 0 1 2 1 1 2 8 0 3 5 8  *
+   * KYRILLOSWALIDRADI@HOTMAIL.COM                          *
+   * WWW.FACEBOOK.COM/KyrillosWalid                         *
+   * WWW.GITHUB.COM/KyrillosWalid                           *
+   **********************************************************
+   */
 	int x;
 	cin>>x;
 	if((x%2==0)&&(x>2)){
